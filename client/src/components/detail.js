1e3564fbf9a3672705fa43670e208938ac48fac9
Matthew Van Schellebeeck <mvanschellebeeck@gmail.com>
2019-07-21 16:18:44 +1000

    almost working

---
+++ client/src/components/detail.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+
+export default class Detail extends Component {
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    const divStyle = {
+      width: '40%',
+      float: 'right',
+      padding: '20px'
+    }
+    const title = "Victorian Desalination Plant";
+    const description = "Some description";
+    
+    //const { title, description, icon } = this.props.details;
+    const data = [{'property': 'capacity', 'value': '500GL'}, {'property': 'energy usage', 'value': '20 something'},
+    {'property': 'skrr', 'value': 'dide'}];
+
+    if (title) {
+      return (
+        <div style={divStyle}>
+          <h1><b>{title}</b></h1>
+          <p>{description}</p>
+          <div>
+            <ReactTable
+              data={data}
+              columns={[
+                {
+                  columns: [
+                    {
+                      Header: "Property",
+                      accessor: "property"
+                    }
+                  ]
+                },
+                {
+                  columns: [
+                    {
+                      Header: "Value",
+                      accessor: "value"
+                    }
+                  ]
+                }
+              ]}
+              defaultPageSize={10}
+              className="-striped -highlight"
+            />
+            <br />
+            </div>  
+        </div>
+      );
+    }
+    else {
+      return (<div style={divStyle}><p>Click a desalination plant for details</p></div>);
+    }
+  }
+}
+//https://docs.google.com/spreadsheets/d/1ByXhNNXjQsJmthiWwn4cfgId32rdCRY6L6rH0R-B20U/edit#gid=0
---
+++ client/src/index.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import mapboxgl from 'mapbox-gl'
+import Detail from './components/detail'
+import registerServiceWorker from './registerServiceWorker';
+
+const DESALINATION_PLANTS = 'desalination-plants';
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+export default class App extends React.Component {
+  tooltipContainer;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+      icon:''
+    }
+  }
+
+  fetchPlantCoordinates() {
+    // temporarily hard-coded
+    const geojson = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [145.526288, -38.588381]
+          },
+          properties: {
+            title: 'Victorian Desalination plant',
+            description: 'Add short description for this desalination plant',
+            icon: 'monument'
+          }
+        }
+      ]
+    }
+    return geojson;
+  }
+
+
+  componentDidMount() {
+    this.currentlyHoveredFeatures = {};
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      //    center: [133.7751, -25.2744],
+      bounds: [[109.338953078, -45.6345972634], [158.569469029, -8.6681857235]],
+      maxBounds: [[109.338953078, -45.6345972634], [158.569469029, -8.6681857235]],    
+    });
+
+    const geojson = this.fetchPlantCoordinates();
+    map.on('load', () => {
+      map.addLayer({
+        "id": "desalination-plants",
+        "type": "symbol",
+        "source": {
+          "type": "geojson",
+          "data": {
+            "type": "FeatureCollection",
+            "features": geojson.features
+          }
+        },
+        "layout": {
+          "icon-image": "{icon}-15",
+          "text-field": "{title}",
+          "text-offset": [0, 0.6],
+          "text-anchor": "top",
+          "text-size": 14,
+          "icon-allow-overlap": true
+        }
+      })
+    });
+
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
+    });
+
+    map.on('mouseenter', DESALINATION_PLANTS, (e) => {
+      map.getCanvas().style.cursor = 'pointer';
+
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const description = e.features[0].properties.description;
+      this.currentlyHoveredFeatures = e.features[0].properties;
+
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      popup.setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+    });
+
+    map.on('mouseleave', DESALINATION_PLANTS, () => {
+      map.getCanvas().style.cursor = '';
+      popup.remove();
+    });
+
+    map.on('click', DESALINATION_PLANTS, (e) => {
+      this.setState(this.currentlyHoveredFeatures);
+    });
+
+  }
+
+  render() {
+    const divStyle = {
+      width: '60%'
+    }
+
+    return (
+      <div>
+        <div style={divStyle} ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+        <Detail details={this.state}/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
