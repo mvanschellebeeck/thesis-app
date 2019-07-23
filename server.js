12040de5795c91130534f73bcbec0f65406c057a
Matthew Van Schellebeeck <mvanschellebeeck@gmail.com>
2019-07-23 20:17:38 +1000

    Add plant get parameter to server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 var global_plants = {};
 app.use(express.json());
 app.use(express.urlencoded());
+
 var GoogleSpreadsheet = require("google-spreadsheet");
 const doc = new GoogleSpreadsheet(
     "1ByXhNNXjQsJmthiWwn4cfgId32rdCRY6L6rH0R-B20U"
@@ -14,6 +15,26 @@ app.post("/api/customers", (req, res) => {
     res.json(global_plants[plant]);
 });
 
+app.get("/api/specificplant", (req, res) => {
+    // make an option to request specific properties
+    const requested_plant = req.body.plant;
+    if (!Object.keys(global_plants).length) {
+        return res.status(400).json({
+            error: 'global_plants variable not loaded yet'
+        });
+    }
+    else if (!(requested_plant in global_plants)) { 
+        return res.status(400).json({
+            error: `${requested_plant} is not a valid plant name`
+        });
+    }
+    else {
+        res.json(global_plants[requested_plant]);
+    }
+});
+
+// replace with async await for cleaner code
+// extract sheet.getRows into function with cleaner interface
 app.get("/api/plants", (req, res) => {
     const plants = {};
     doc.getInfo((err, info) => {
