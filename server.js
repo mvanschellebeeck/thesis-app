edc160888291a2fa5c1c998062fa67f7962bdaf3
Matthew Van Schellebeeck <mvanschellebeeck@gmail.com>
2019-08-12 12:28:15 +1000

    Add react bootstrap framework

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+
+// temporary gross global variable stored on the server until some sort of databse is used
 var global_plants = {};
 app.use(express.json());
 app.use(express.urlencoded());
