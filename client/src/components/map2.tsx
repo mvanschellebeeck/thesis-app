6a71faa0a0e28ae361768a4432be3794970ee9eb
Matthew Van Schellebeeck <mvanschellebeeck@gmail.com>
2019-10-31 09:14:51 +1100

    State clustering work

--- client/src/components/map2.tsx
+++ client/src/components/map2.tsx
@@ -5,8 +5,8 @@ import '../index.css';
 const GAB_ID = 'great-artesian-basin';
 const MDB_ID = 'murray-darling-basin';
 
-//const states = ['NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA'];
-const states = ['NSW'];
+const states = ['NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA'];
+//const states = ['NSW'];
 
 process.env.REACT_APP_MAPBOX_TOKEN === undefined
   ? console.log('Please add REACT_APP_MAPBOX_TOKEN to .env file.')
@@ -51,19 +51,71 @@ export default class Map extends Component {
           });
 
           states.map(state => {
-            map.addLayer({
-             id: `${state}_bores`,
-             layout: {
-              'icon-allow-overlap': true,
-              'icon-image': '{icon}',
-            },
-            source: {
+            map.addSource(`${state}_bores`, {
+              type: "geojson",
+              // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
+              // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
               data: `../geojson/bores/${state}_simple.geojson`,
-              type: 'geojson',
-            },
-            type: 'symbol',
+              cluster: true,
+              clusterMaxZoom: 14, // Max zoom to cluster points on
+              clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
+            });
+           map.addLayer({
+              id: `${state}_clusters`,
+              type: "circle",
+              source: `${state}_bores`,
+              filter: ["has", "point_count"],
+              paint: {
+                // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+                // with three steps to implement three types of circles:
+                //   * Blue, 20px circles when point count is less than 100
+                //   * Yellow, 30px circles when point count is between 100 and 750
+                //   * Pink, 40px circles when point count is greater than or equal to 750
+                "circle-color": [
+                "step",
+                ["get", "point_count"],
+                "#51bbd6",
+                100,
+                "#f1f075",
+                750,
+                "#f28cb1"
+                ],
+                "circle-radius": [
+                "step",
+                ["get", "point_count"],
+                20,
+                100,
+                30,
+                750,
+                40
+                ]
+              }
+          });
+
+          map.addLayer({
+              id: `${state}_cluster-count`,
+              type: "symbol",
+              source:`${state}_bores`,
+              filter: ["has", "point_count"],
+              layout: {
+                "text-field": "{point_count_abbreviated}",
+                "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+                "text-size": 12
+              }
+            });
+            
+             map.addLayer({
+              id: `${state}_unclustered-point`,
+              type: "symbol",
+              source:`${state}_bores`,
+              filter: ["!", ["has", "point_count"]],
+              layout: {
+                'icon-allow-overlap': true,
+                'icon-image': '{icon}'
+              }
             });
           })
+          
        })
  }
 
