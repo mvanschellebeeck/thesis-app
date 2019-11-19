62342e68e248ebb7da74aa726921a8818d86cbff
Matthew Van Schellebeeck <mvanschellebeeck@gmail.com>
2019-11-19 23:40:47 +1100

    Clean up packages - remove linter files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import Home from './pages/Home';
-import Navigation from './pages/Navigation';
 import PageNotFound from './pages/PageNotFound';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Desalination from './pages/Desalination';
@@ -12,10 +10,9 @@ export default function App() {
   return (
     <Router>
       <div>
-        {/*          <Navigation /> */}
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/inland-desalination" component={Desalination} />
+          <Route path="/" component={Desalination} />
+          <Route path="/desalination" component={Desalination} />
           <Route component={PageNotFound} />
         </Switch>
       </div>
--- server/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  mongo: {
-    url: 'mongodb://localhost:27017',
-    dbs: {
-      regional_db: 'regional',
-      technologies_db: 'technologies',
-    },
-  },
-};
